3f68d45573033e504affaa758e34b86a9395458f
Abrar Al-Shaer <abrar@email.unc.edu>
2018-12-05 11:53:05 -0500

    Update powerAnalysis_ANOVAs_estimateMean-SD_fromIQR.R

--- powerAnalysis_ANOVAs_estimateMean-SD_fromIQR.R
+++ powerAnalysis_ANOVAs_estimateMean-SD_fromIQR.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/Abrar/Dropbox/UNC_OneDrive/Shaikh Lab/Simulation Power Ananlysis/")
+setwd("C:/Users/Abrar/Dropbox/UNC_OneDrive/Shaikh Lab/Raz_Grant Nov2018/")
 rm(list=ls())
 library(pwr)
 library(pwr2)
@@ -32,10 +32,19 @@ sd_pre <- (q3_pre - q1_pre) / (2 * (qnorm((0.75 * n_pre - 0.125) / (n_pre + 0.25
 #calculating effect size for detecting HAI titers 30 days post vaccination
 power <- (mean_pre - mean_post) / sqrt(((sd_pre^2 + sd_post^2)/2))
 
+#power analysis for T-test
+pwr.t.test(n = 228, d = 0.3, sig.level = 0.05, type = c("two.sample"))
+#You can specify alternative="two.sided", "less", or "greater" to indicate a two-tailed, or one-tailed test. A two tailed test is the default
+
+#power analysis for One-way ANOVA
+pwr.anova.test(k = 2, n = NULL, f = 0.2, sig.level = 0.05, power = 0.8)
+#where k is the number of groups and n is the common sample size in each group, f = effect size
+#effect size options are the same as below.
+
 #power analysis for 2-way ANOVA
 #A & B are # of groups in factors A and B. We have 2 groups (DHA & control) and male and female for each of the 2 groups. 
 #So we have 2 factors (M & F) with 2 groups each. 
-ss.2way(a=3,b=3,alpha=0.05,beta=0.2,f.A = 0.25, f.B = 0.25, B=100)
+ss.2way(a=2,b=2,alpha=0.05,beta=0.2,f.A = 0.2, f.B = 0.2, B=100)
 #Cohen's d 2-way ANOVA effect sizes:
 #0.1 small effect size 
 #0.25 medium effect size
@@ -47,7 +56,7 @@ main.eff1 <- list(name = "Sex", levels = 2, eta.sq = "med")
 main.eff2 <- list(name = "SNPs", levels = 3, eta.sq = "med")
 main.eff3 <- list(name = "DHA", levels = 2, eta.sq = "med")
 main.eff4 <- list(name = "BMI", levels = 3, eta.sq = "med")
-n.multiway(iv1 = main.eff1, iv2 = main.eff2, iv3 = main.eff3, interaction.eta2 = "med", result = "all") #med is = 0.06 which is assuming a moderate effect size (med) for the interaction terms 
+n.multiway(iv1 = main.eff1, iv2 = main.eff2, iv3 = main.eff3, interaction.eta2 = "med", result = "all", power = 0.80) #med is = 0.06 which is assuming a moderate effect size (med) for the interaction terms 
 #you can specify the power you want with power = 0.90 or any other number. 
 #4-way ANOVA
 n.multiway(iv1 = main.eff1, iv2 = main.eff2, iv3 = main.eff3, iv4 = main.eff4, interaction.eta2 = 0.04, result = "all") #0.04 is estimating a slightly below moderate effect size for the interaction terms
